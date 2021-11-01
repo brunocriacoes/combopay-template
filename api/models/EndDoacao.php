15d8b2243e84232054d02d5a88447eb1c204c548
Victorfer18 <victorfernandomagalhaes@gmail.com>
2021-11-01 10:19:49 -0300

    fix

--- api/models/EndDoacao.php
+++ api/models/EndDoacao.php
@@ -38,7 +38,7 @@ class EndDoacao
         ";
         $title = "Doação esperando o  PIX";
         if ($status == 'error') return null;
-        if ($tipo == 'boleto' || $tipo == 'cartao') return null;
+        if ($tipo == 'boleto' || $tipo == 'card') return null;
         EndDoacao::send($instituicao_id, $conteudo, $to, $title);
     }
 
