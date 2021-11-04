7f15200649d38597206194a4f95940afde29affb
Victorfer18 <victorfernandomagalhaes@gmail.com>
2021-11-04 12:10:16 -0300

    fix

--- api/models/Doador.php
+++ api/models/Doador.php
@@ -17,39 +17,62 @@ class Doador implements IDoador
         $banco = new BancoM;
         $sql = "SELECT * FROM doador WHERE cpf='$cpf' or email='$email'";
         $guard = $banco->query($sql);
-        var_dump($guard);
         return $sql;
     }
     
-    static function teste()
-    {
-            Doador::exist("02754669906", "");
-    }
 
     public function get_by_costumer_id(int $id): array
     {
-        
+        $banco = new BancoM();
+        $sql = "SELECT * FROM doador WHERE codigo_pagarme='$id'";
+        $guard = $banco->query($sql);
+                      
     }
 
     public function add_costumer_id(int $id, int $costumer_id_pagar_me): void
     {
-                
-    }
-
-    public function get_by_id(int $id): array
-    {}
-
-    public function get_by_email(string $email): array
-    {}
-
-    public function get_by_cpf(string $cpf): array
-    {}
-    public function create(string $name, string $email, int $external_id, array $phone_numbers, array $cpf, string $senha = '', string $genero = '', int $costumer_id = 0): int
-    {
-        $banco = new BancoM();
+        var_dump($guard);
         
     }
 
+    public function get_by_id(int $id): array
+    {
+        $banco = new BancoM(); 
+        $sql = "SELECT * FROM doador WHERE id='$id'";
+        $guard = $banco->query($sql);
+        
+    }
+
+    public function get_by_email(string $email): array
+    {
+        $banco = new BancoM();
+        $sql = "SELECT * FROM doador WHERE email='$email'";
+        $guard = $banco->query($sql);
+    }
+
+    public function get_by_cpf(string $cpf): array
+    {
+        $banco = new BancoM();
+        $sql = "SELECT * FROM doador WHERE cpf=$cpf";
+        $guard = $banco->query($sql);
+        
+        
+    }
+    public function create(string $name, string $email, int $external_id, array $phone_numbers, array $cpf, string $senha = '', string $genero = '', int $costumer_id = 0): int
+    {
+        $banco = new BancoM();
+        $sql = "INSERT INTO doador";
+        $sql .= "(id, nome, email, telefone, cpf, sexo, codigo_pagarme)";
+        $sql .= "VALUES";
+        $sql .= "('$external_id', '$name', '$email', '$phone_numbers', '$cpf', '$genero', '$costumer_id')";
+        $banco->exec($sql);
+       
+    }
+
+    static function teste()
+    {
+        Doador::get_by_cpf("63203995972");
+    }
 
 
 
